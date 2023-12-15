791fb9759b9a10b55a8aa981acd520fb2d1d6c19
Lukasz <l.bdnski@gmail.com>
2023-12-15 16:07:18 +0100

    .

--- src/components/RenderSlots.js
+++ src/components/RenderSlots.js
@@ -48,6 +48,7 @@ const RenderSlots = (props) => {
                         />
                     )
                 })
+                
 
                 return (
                     <ContainerDay>
